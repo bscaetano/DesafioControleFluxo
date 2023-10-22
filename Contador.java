1a707d1572b0f3f7ca6bc2151bc286f96845305e
bscaetano <139719932+bscaetano@users.noreply.github.com>
2023-10-22 17:53:25 -0300

    Create Contador.java

---
+++ Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = Integer.parseInt(terminal.nextLine());
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = Integer.parseInt(terminal.nextLine());
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static class ParametrosInvalidosException extends Exception {
+
+        public ParametrosInvalidosException(String message) {
+            super(message);
+        }
+    }
+}
